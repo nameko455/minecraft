9330fe61b0139da52640f09322a9852e1aabdef4
nameko455 <nameko455@gmail.com>
2016-01-06 11:30:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install openjdk-7-jre -y
-mkdir minecra
-cd minecra
+mkdir minecraft
+cd minecraft
 wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
 java -jar Minecraft.jar -Xmx1024M -Xms512M -Xmn1024M
