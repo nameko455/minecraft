a920f91e02e05f12601e403de08489485ab111b0
fai1618 <fai1.618033@gmail.com>
2016-01-06 11:01:47 +0900

    make sh file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get install openjdk-7-jre -y
+mkdir minecra
+cd minecra
+wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
+java -jar Minecraft.jar -Xmx1024M -Xms512M -Xmn1024M
